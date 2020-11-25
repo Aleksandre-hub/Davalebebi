f09a2b352c8427986372e940e3302cb41229f2b3
Aleksandre-hub <aleksandre.tvaradze.1@btu.edu.ge>
2020-11-26 00:19:18 +0400

    added something ooops

--- bugsandfixes/app/src/main/java/com/example/bugsandfixes/MainActivity.java
+++ bugsandfixes/app/src/main/java/com/example/bugsandfixes/MainActivity.java
@@ -2,9 +2,25 @@ package com.example.bugsandfixes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void install (View view)
+    {
+        Button instali = (Button) findViewById(R.id.install);
+        Intent instalaciaintent = new Intent(this, instalacia.class);
+        startActivity(instalaciaintent);
+    }
+
+    public void problemebi (View view)
+    {
+       Intent fixebiii = new Intent(this, fixebi.class);
+       startActivity(fixebiii);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ fixebi/app/src/main/java/com/example/fixebi/fsaf.java
@@ -0,0 +1,14 @@
+package com.example.fixebi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fsaf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fsaf);
+    }
+}
