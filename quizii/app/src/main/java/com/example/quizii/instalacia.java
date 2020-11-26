fae6114284409b5127f937785448571daa25e356
Aleksandre-hub <aleksandre.tvaradze.1@btu.edu.ge>
2020-11-26 18:31:14 +0400

    added pictures

--- quizii/app/src/main/java/com/example/quizii/instalacia.java
+++ quizii/app/src/main/java/com/example/quizii/instalacia.java
@@ -2,9 +2,23 @@ package com.example.quizii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class instalacia extends AppCompatActivity {
+
+    public void finish (View view)
+    {
+        finish();
+    }
+
+    public void link (View view)
+    {
+        Intent linki = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.developer.android.com.kh"));
+        startActivity(linki);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
