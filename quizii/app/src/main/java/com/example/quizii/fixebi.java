c35692b0bc718a52e57d0f744e3ca5348b71d664
Aleksandre-hub <aleksandre.tvaradze.1@btu.edu.ge>
2020-11-26 20:22:38 +0400

    added all pictures to drawable

--- quizii/app/src/main/java/com/example/quizii/fixebi.java
+++ quizii/app/src/main/java/com/example/quizii/fixebi.java
@@ -3,8 +3,14 @@ package com.example.quizii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class fixebi extends AppCompatActivity {
+
+    public void finishii (View view)
+    {
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
